cd3bb2c2491b8919d63fa725a08cdaa67bb971e7
padawan95 <zeli.armel@gmail.com>
2021-06-11 12:31:49 +0200

    Fix somes errors

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -17,9 +17,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Workflow\Registry;
-use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
-use Symfony\Component\HttpKernel\KernelInterface;
-
 
 /**
  * @Route("/admin")
